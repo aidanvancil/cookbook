e24fd97ecf9339339990178cd7d83c21d952f7b1
aidanvancil <aidanv0324@gmail.com>
2023-11-27 17:31:39 -0800

    add: views login

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,61 +1,60 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as log, logout as auth_logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
 
 def errors(request):
     context = {
         'error': 404
     }
     return render(request, 'errors.html', context)
-
-def master(request):
-    context = {
-        
-    }
-    return render(request, 'master.html', context)
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
 
 def signup(request):
     if request.method == 'POST':
-        first_name = request.POST.get('firstname')
-        last_name = request.POST.get('lastname')
-        email = request.POST.get('email')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        context = {
-            
-        }
-
-        return render(request, 'signup.html', context)
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            log(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
 
     context = {
-        
+        'form': form,
     }
-
     return render(request, 'signup.html', context)
 
 
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:    
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             log(request, user)
             return redirect('home')
-        else:
-            error_message = "Username or password is incorrect."
-            context = {
-                'error_message': error_message,
-            }
-            
-            return render(request, 'login.html', context)
-    
-    
+    else:
+        form = LoginForm()
+
     context = {
-        
+        'form': form,
     }
-    
     return render(request, 'login.html', context)
 
 @login_required(login_url='login')
@@ -66,6 +65,13 @@ def logout(request):
 @login_required(login_url='login')
 def home(request):
     context = {
-        
-    } 
+
+    }
     return render(request, 'home.html', context)
+
+@login_required(login_url='login')
+def bookmarks(request):
+    context = {
+
+    }
+    return render(request, 'bookmarks.html', context)
