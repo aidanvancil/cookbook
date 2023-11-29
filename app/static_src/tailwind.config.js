622ed0a302b996074bad0f1bec86439053f5381b
aidanvancil <aidanv0324@gmail.com>
2023-11-29 01:17:18 -0800

    package-add: tailwind scrollbar config

--- app/static_src/tailwind.config.js
+++ app/static_src/tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
          * Adjust the following line to match your project structure.
          */
         '../../**/templates/**/*.html',
+        "../templates/**/*.{html,js}",
 
         /**
          * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
@@ -54,5 +55,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('tailwind-scrollbar'),
     ],
 }
