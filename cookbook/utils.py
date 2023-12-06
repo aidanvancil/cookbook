c502dd1f339ba2adc151d4893037d723c720f84b
aidanvancil <aidanv0324@gmail.com>
2023-12-05 17:11:30 -0800

    remove: print statements in utils

--- cookbook/utils.py
+++ cookbook/utils.py
@@ -1,3 +1,4 @@
+# External Libs
 import requests
 from decouple import config
 from urllib.parse import quote
@@ -21,8 +22,7 @@ def get_recipe_by_url(recipe_url):
 
     api_url = f'https://api.edamam.com/api/recipes/v2/by-uri?app_id={API_APP_ID}&app_key={API_APP_KEY}&uri={encoded_recipe_url}&type=public'
     response = requests.get(api_url)
-    print(response.json())
-
+    
     if response.status_code == 200:
         return response.json().get('hits', {})[0]
     else:
